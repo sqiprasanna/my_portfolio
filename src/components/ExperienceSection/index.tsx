7c293407bd9cf445d7fc6914a8ba182ef5ea65e4
sqiprasanna <sai1999prasanna@gmail.com>
2024-11-14 00:30:04 -0800

    RSI added to experience

--- src/components/ExperienceSection/index.tsx
+++ src/components/ExperienceSection/index.tsx
@@ -8,6 +8,30 @@ function ExperienceSection() {
   return (
     <AppSection headerTxt={"experience"}>
       <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+      <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+              <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+            </svg>
+          </div>
+          <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div className="mb-4">
+              <div className="font-bold text-slate-900">Data Scientist II </div>
+              <div className="flex justify-between items-center">
+                <div className="font-bold text-slate-900">Republic Services Inc.</div>
+                <time className="font-caveat font-medium text-amber-500">Sept 2024 – Present</time>
+              </div>
+            </div>
+            <ul className="list-disc text-slate-500 space-y-1 pl-5">
+              <li>Developed customer service chatbots using Retrieval-Augmented Generation (RAG) architecture with AWS infrastructure, integrating models like Claude, Mistral, and LLaMA via AWS Bedrock, and utilizing GraphRAG.</li>
+              <li>Deployed and monitored an end-to-end pipeline using AWS services like Knowledge bases, Lambda, Step Functions, DynamoDB, OpenSearch, Grafana, CloudWatch, and ECR, optimizing document management and data retrieval.</li>
+              <li>Built an AI-driven 'Next Best Actions' solution, boosting customer engagement by 30% and reducing response time by 25%, enhancing satisfaction and retention.</li>
+              <li>Cut latency in retrieving customer service answers by 50%, reducing call representative workload by 20% monthly.</li>
+              <li>Evaluated pipeline performance with RAGAS, enhancing model efficiency by 10%.</li>
+              <li>Scaled chatbot applications across various operational areas at Republic Services, influencing a $20M budget.</li>
+            </ul>
+          </div>
+        </div>
         <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
@@ -16,17 +40,17 @@ function ExperienceSection() {
           </div>
           <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
             <div className="mb-4">
-              <div className="font-bold text-slate-900">Data Scientist</div>
+              <div className="font-bold text-slate-900">Senior Data Scientist</div>
               <div className="flex justify-between items-center">
                 <div className="font-bold text-slate-900">BobiHealth</div>
                 <time className="font-caveat font-medium text-amber-500">Aug 2023 – May 2024</time>
               </div>
             </div>
             <ul className="list-disc text-slate-500 space-y-1 pl-5">
-              <li>Developed an end-to-end NLP-driven chatbot leveraging Hugging Face Transformers, Langchain, GPT-4, RAG, RLHF, ChromaDB, and FastAPI, boosting user engagement with 90% retrieval accuracy. Conducted A/B testing to evaluate the effectiveness of different chatbot interactions.</li>
-              <li>Designed an AI model for pregnancy risk analysis (90% accuracy) using Random Forest and collected data for analysis from public health datasets. Engineered data pipelines and provided predictive analytics with Azure Databricks.</li>
+              <li>Developed an end-to-end NLP-driven chatbot leveraging Hugging Face Transformers, Langchain, GPT-4, RAG, RLHF, ChromaDB, and FastAPI, boosting user engagement with 90% retrieval accuracy.</li>
+              <li>Designed an AI model for pregnancy risk analysis (90% accuracy) using Random Forest, and collected data for analysis from public health datasets. Engineered data pipelines and provided predictive analytics with Azure Databricks.</li>
               <li>Orchestrated Airflow workflows for model training and deployment; optimized CI/CD pipelines and Python data modeling to AWS S3, increasing agility by 15%. Incorporated GDPR principles through FMEA ensuring compliance and data protection.</li>
-              <li>Utilized R and dplyr for advanced data analytics methodologies to analyze pregnancy risk datasets, writing large-scale ad-hoc SQL queries and creating interactive visualizations using PowerBI, Tableau, and extracting KPI metrics.</li>
+              <li>Utilized R and dplyr for advanced data analytics, analyzed pregnancy risk datasets, performed large-scale ad-hoc SQL queries, and created interactive visualizations using PowerBI, Tableau to extract KPI metrics.</li>
             </ul>
           </div>
         </div>
@@ -56,7 +80,7 @@ function ExperienceSection() {
           </div>
         </div>
 
-        <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+        {/* <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
               <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
@@ -76,7 +100,7 @@ function ExperienceSection() {
               <li>Employed advanced GAN architectures, including DcGAN and StyleGAN, and trained the models on high-end GPUs to produce high-fidelity facial images with precise detail and variability.</li>
             </ul>
           </div>
-        </div>
+        </div> */}
 
         <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
@@ -88,8 +112,8 @@ function ExperienceSection() {
             <div className="mb-4">
               <div className="font-bold text-slate-900">Data Scientist</div>
               <div className="flex justify-between items-center">
-                <div className="font-bold text-slate-900">Accenture</div>
-                <time className="font-caveat font-medium text-amber-500">Jul 2019 – Jun 2020</time>
+                <div className="font-bold text-slate-900">Flutura Decision Sciences and Analytics</div>
+                <time className="font-caveat font-medium text-amber-500">Jan 2020 – Jun 2020</time>
               </div>
             </div>
             <ul className="list-disc text-slate-500 space-y-1 pl-5">
@@ -99,8 +123,32 @@ function ExperienceSection() {
             </ul>
           </div>
         </div>
-
+        
         <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+          <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+              <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+            </svg>
+          </div>
+          <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+            <div className="mb-4">
+              <div className="font-bold text-slate-900">Data Scientist</div>
+              <div className="flex justify-between items-center">
+                <div className="font-bold text-slate-900">Freelancing</div>
+                <time className="font-caveat font-medium text-amber-500">Aug 2019 – Dec 2019</time>
+              </div>
+            </div>
+            <ul className="list-disc text-slate-500 space-y-1 pl-5">
+              <li>	Led photorealistic face generation project using GANs (DcGAN, StyleGAN), enhancing diversity in synthetic image datasets.</li>
+              <li>Curated a dataset of 20,000 Indian facial images via web scraping, ensuring diverse feature representation.</li>
+              <li>Trained GAN models on high-end GPUs NVIDIA A100s on AWS EC2, achieving high-fidelity image generation.</li>
+            </ul>
+          </div>
+        </div>
+
+
+
+        {/* <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
               <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
@@ -121,7 +169,7 @@ function ExperienceSection() {
               <li>Developed a Hospital Management System using MERN stack, integrating Redux and TailwindCSS for seamless UI/UX. Integrated AWS Elastic Load Balancing to efficiently handle over 5,000 daily active users, ensuring robust scalability and uninterrupted access to critical patient management features.</li>
             </ul>
           </div>
-        </div>
+        </div> */}
 
         <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
           <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
@@ -133,8 +181,8 @@ function ExperienceSection() {
             <div className="mb-4">
               <div className="font-bold text-slate-900">Data Science Intern</div>
               <div className="flex justify-between items-center">
-                <div className="font-bold text-slate-900">KPMG</div>
-                <time className="font-caveat font-medium text-amber-500">Jan 2019 – Jun 2019</time>
+                <div className="font-bold text-slate-900">JotArthur Web Services </div>
+                <time className="font-caveat font-medium text-amber-500">May 2019 – July 2019</time>
               </div>
             </div>
             <ul className="list-disc text-slate-500 space-y-1 pl-5">
