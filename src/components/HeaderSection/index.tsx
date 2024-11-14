34b572c2c4e40727f48968b184a7a1a5e53befe0
sqiprasanna <sai1999prasanna@gmail.com>
2024-11-14 00:55:12 -0800

    resume added

--- src/components/HeaderSection/index.tsx
+++ src/components/HeaderSection/index.tsx
@@ -51,7 +51,7 @@ function HeaderSection() {
               ariaLabel=""
               buttonType="secondary"
               onClick={() => {
-                window.open("https://drive.google.com/file/d/1mnbT9Bt6gtE-FiLQjcvLyv-f_vNeB8f6/view?usp=sharing", '_blank');
+                window.open("https://drive.google.com/file/d/1X9RfN6sqNyBH5ErnNiQLwUL8TlYau48P/view?usp=sharing", '_blank');
               }}
               buttonText="See My Resume"
             />
